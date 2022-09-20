2e43ac75d7be7e8ba6e1ce31c50ca9c8afddba9d
minsu_choi <mine52100@naver.com>
2022-09-21 07:54:07 +0900

    학습: Ch03_1 삽입정렬

---
+++ 03_1_Array/InsertionSort/insertionSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(char A[], int n)
+{
+    for (int i = 1; i < n; i++)
+    { // target선택
+        char cur = A[i];
+        int k = i - 1;
+        while ((k >= 0) && (A[k] > cur))
+        {
+            A[k + 1] = A[k];
+            k--;
+        }
+        A[k + 1] = cur;
+    }
+}
+
+int main()
+{
+    char A[] = {'A', 'E', 'C', 'D', 'B'};
+    cout << "정렬 전:";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i];
+    }
+    cout << endl;
+
+    insertionSort(A, 5);
+
+    cout << "정렬 후:";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i];
+    }
+    cout << endl;
+}
