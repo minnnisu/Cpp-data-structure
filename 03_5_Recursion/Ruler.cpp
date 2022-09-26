c5cf4944daba7648d4703fb20231903348ba04b0
minsu_choi <mine52100@naver.com>
2022-09-26 19:34:36 +0900

    학습: Ch03_5 Recursion InchRuler

---
+++ 03_5_Recursion/Ruler.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+
+//눈금을 만드는 함수
+string makeTicks(int n){
+    if(n == 1){
+        return "-";
+    }else{
+        string tick = makeTicks(n-1);
+        cout << tick << endl;
+        makeTicks(n-1);
+        
+        return tick + "-";
+    }
+}
+
+//원하는 크기의 하나의 눈금을 만드는 함수
+string makeOneTick(int tick){
+    if(tick == 0){
+        return "-";
+    }else{
+        return makeOneTick(tick-1) + "-";
+    }
+}
+
+//인치를 만드는 함수
+void makeRuler(int inch, int tick){
+    if(inch == 0){
+        cout << makeOneTick(tick) << " " << inch << endl;
+        return;
+    }else{
+        makeRuler(inch-1, tick);
+        cout << makeTicks(tick) << " " << inch << endl; 
+
+    }
+}
+
+int main(){
+    int inch; //인치
+    int ticks; //눈금 개수
+
+    cout << "enter inch : ";
+    cin >> inch;
+    cout << "enter major tick length : ";
+    cin >> ticks;
+    makeRuler(inch, ticks);
+
+    return 0;
+}
